84ef043aa6a0142e6a06e4c955e06c95ca118ff3
Kseniya <kseniya.b13@bk.ru>
2024-05-07 23:41:05 +0300

    Initial commit

---
+++ music_service_diagram.sql
@@ -0,0 +1,46 @@
+create table if not exists Genre (
+	id serial primary key,
+	name varchar (100) not null
+);
+
+create table if not exists Artist (
+	id serial primary key,
+	name varchar (200) not null
+);
+
+create table if not exists Album (
+	id serial primary key,
+	name varchar (100) not null,
+	year_of_release date not null
+);
+
+create table if not exists Track (
+	id serial primary key,
+	name varchar (100) not null,
+	duration decimal (3,2) not null,
+	album integer not null references Album(id)
+);
+
+create table if not exists Collection (
+	id serial primary key,
+	name varchar (100) not null,
+	year_of_release date not null
+);
+
+create table if not exists Genre_Artist (
+	id serial primary key,
+	artist_id integer not null references Artist(id),
+	genre_id integer not null references Genre(id)
+);
+
+create table if not exists  Album_Artist (
+	id serial primary key,
+	artist_id integer not null references Artist(id),
+	album_id integer not null references Album(id)
+);
+
+create table if not exists  Track_Collection (
+	id serial primary key,
+	track_id integer not null references Track(id),
+	collection_id integer not null references Collection(id)
+);
